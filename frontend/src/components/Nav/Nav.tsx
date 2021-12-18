e13d8047b8814a1c4e9488bf8a12924aea56e6c9
zack-ashen <zachary.h.a@gmail.com>
2021-12-18 09:46:37 -0500

    update nav

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,30 +1,19 @@
-import { NavDisplay } from '../../types'
-import { useHistory, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './Nav.css'
 
 type NavProps = {
-  navDisplay: NavDisplay;
+  authButton: JSX.Element;
 }
 
-const Nav = ({ navDisplay }: NavProps) => {
-  const history = useHistory();
+const Nav = ({ authButton }: NavProps) => (
+  <nav>
+    <Link to="/"><h1>AnyStudy</h1></Link>
+    <div className="navDetails">
+      {authButton}
+    </div>
+  </nav>
+);
 
-  return (
-    <nav>
-      <Link to="/"><h1>AnyStudy</h1></Link>
-      <div className="navDetails">
-        {navDisplay === NavDisplay.SIGN_IN &&
-          <button 
-            value="Sign In" 
-            className="signInButton"
-            onClick={() => history.push('/signin')}>
-              Sign In
-            </button>
-        }
-      </div>
-    </nav>
-  );
-}
 
 export default Nav;
