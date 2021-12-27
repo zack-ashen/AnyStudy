420639fda2c2532283a19f60ec24bc580a887e57
zack-ashen <zachary.h.a@gmail.com>
2021-12-27 12:23:38 -0500

    User form slider works on all 5 steps

--- frontend/src/components/Stepper/Stepper.tsx
+++ frontend/src/components/Stepper/Stepper.tsx
@@ -1,4 +1,4 @@
-import { slider1, slider2 } from '../../assets';
+import { slider1, slider2, slider3, slider4, slider5 } from '../../assets';
 
 import './Stepper.css';
 
@@ -13,6 +13,12 @@ const Stepper = ({ step }: StepperProps) => {
       return <object type="image/svg+xml" data={slider1} className="Stepper">svg-animation</object>
     case 2:
       return <object type="image/svg+xml" data={slider2} className="Stepper">svg-animation</object>
+    case 3:
+      return <object type="image/svg+xml" data={slider3} className="Stepper">svg-animation</object>
+    case 4:
+      return <object type="image/svg+xml" data={slider4} className="Stepper">svg-animation</object>
+    case 5:
+      return <object type="image/svg+xml" data={slider5} className="Stepper">svg-animation</object>
     default:
       return <></>
   }
