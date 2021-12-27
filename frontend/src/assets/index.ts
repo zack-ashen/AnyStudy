88cf84fa81a5db27b5e8600a895c1907d228fb26
zack-ashen <zachary.h.a@gmail.com>
2021-12-27 12:24:14 -0500

    update slider names

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,2 +1,5 @@
 export { default as slider1 } from "./slider_1.svg";
 export { default as slider2 } from "./slider_2.svg";
+export { default as slider3 } from "./slider_3.svg";
+export { default as slider4 } from "./slider_4.svg";
+export { default as slider5 } from "./slider_5.svg";
